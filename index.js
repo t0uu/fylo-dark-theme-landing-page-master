aa301ea3028bd521cd670da3f256fc44bac88683
t0uu <srtouzet@hotmail.com>
2022-04-09 01:36:03 -0300

    Add ScrollReveal and Mobile First Finish

---
+++ index.js
@@ -0,0 +1,10 @@
+
+// ScrollReveal(options)
+ScrollReveal().reveal('.MainBox .NavList', {delay: 800, easing: 'ease-in'})
+ScrollReveal().reveal('.ContImg img', {delay: 1000, easing: 'ease-in'})
+ScrollReveal().reveal('.SectionOne h1', {delay: 1500, easing: 'ease-out'})
+ScrollReveal().reveal('.SectionOne .Container-Box p', {delay: 1500, easing: 'ease-out'})
+ScrollReveal().reveal('.SectionOne .StartBtn', {delay: 1500, easing: 'ease-out'})
+ScrollReveal().reveal('.SectionOne .Container-Box h2', {delay: 500, easing: 'ease'})
+
+ScrollReveal().reveal('.SectionOne .Container-Box img', {delay: 500, easing: 'ease'})
